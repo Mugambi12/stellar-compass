6d2abe071a7c3a09d436ac98055a52222e2296fe
mugambi12 <silasmungiria16@gmail.com>
2024-03-30 20:08:56 +0300

    Add 'deleted' field to medication model and implement GET method for users

--- server/main.py
+++ server/main.py
@@ -43,6 +43,7 @@ medication_model = api.model(
         'category': fields.String,
         'price': fields.Float,
         'stock_quantity': fields.Integer,
+        'deleted': fields.Boolean,
         'created_at': fields.DateTime,
         'updated_at': fields.DateTime
     }
@@ -51,7 +52,13 @@ medication_model = api.model(
 
 @api.route('/users')
 class UserResource(Resource):
-    @api.expect(user_model)
+
+    @api.marshal_with(user_model)
+    def get(self):
+        """Get all users"""
+        users = User.query.all()
+        return users, 200
+
     @api.marshal_with(user_model)
     def post(self):
         """Create a new user"""
@@ -60,14 +67,9 @@ class UserResource(Resource):
         new_user.save()
         return new_user, 201
 
-    @api.marshal_with(user_model)
-    def get(self):
-        """Get all users"""
-        users = User.query.all()
-        return users, 200
 
 @api.route('/users/<int:id>')
-class MedicationResource(Resource):
+class UserResource(Resource):
 
     @api.marshal_with(user_model)
     def get(self, id):
@@ -94,41 +96,46 @@ class MedicationResource(Resource):
 
 @api.route('/medications')
 class MedicationResource(Resource):
-    @api.marshal_with(medication_model)
 
-    @api.expect(medication_model)
+    @api.marshal_list_with(medication_model)
+    def get(self):
+        """Get all medications"""
+        medications = Medication.query.all()
+        return medications, 200
+
+    @api.marshal_with(medication_model)
     def post(self):
         """Create a new medication"""
         data = request.get_json()
         new_medication = Medication(**data)
         new_medication.save()
-        return {"message": "Medication created successfully"}
-
-    def get(self):
-        """Get all medications"""
-        medications = Medication.query.all()
-        return jsonify(medications)
+        return new_medication, 201
 
 
 @api.route('/medications/<int:id>')
 class MedicationResource(Resource):
+
+    @api.marshal_with(medication_model)
     def get(self, id):
         """Get a medication by id"""
-        medication = Medication.query.get(id)
-        return jsonify(medication)
+        medication = Medication.query.get_or_404(id)
+        return medication, 200
 
+    @api.marshal_with(medication_model)
     def put(self, id):
         """Update a medication by id"""
-        medication = Medication.query.get(id)
-        data = request.json
-        medication.update(**data)
-        return {"message": "Medication updated successfully"}
+        medication_to_update = Medication.query.get_or_404(id)
+        data = request.get_json()
+        medication_to_update.update(**data)
 
+        return medication_to_update, 200
+
+    @api.marshal_with(medication_model)
     def delete(self, id):
         """Delete a medication by id"""
-        medication = Medication.query.get(id)
-        medication.delete()
-        return {"message": "Medication deleted successfully"}
+        medication_to_delete = Medication.query.get_or_404(id)
+        medication_to_delete.delete()
+        return medication_to_delete, 200
 
 
 @app.shell_context_processor
