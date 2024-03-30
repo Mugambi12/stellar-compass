c8bc3fa6cda3dc856b0b35dbb8571e7de991b777
mugambi12 <silasmungiria16@gmail.com>
2024-03-31 01:03:07 +0300

    Update server/run.py to create database tables

--- server/run.py
+++ server/run.py
@@ -1,14 +1,11 @@
 # Path: server/run.py
 
-from main import create_app
+from main import create_app, db
 from config import DevelopmentConfig
 
 
 if __name__ == '__main__':
     app = create_app(DevelopmentConfig)
+    with app.app_context():
+        db.create_all()
     app.run()
-
-#if __name__ == '__main__':
-#    with app.app_context():
-#        db.create_all()
-#    app.run()
