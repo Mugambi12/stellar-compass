a7ede5104dbdbe7f62852500bb632aeb3ed6817a
mugambi12 <silasmungiria16@gmail.com>
2024-04-01 19:08:11 +0300

    Register component form validation handling

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,31 +1,28 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Card, Row, Col } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [contact_info, setContactInfo] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm_password, setConfirmPassword] = useState("");
+  const {
+    register,
+    watch,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const submitForm = () => {
-    console.log("Form submitted");
-    console.log(username);
-    console.log(email);
-    console.log(address);
-    console.log(contact_info);
-    console.log(password);
-    console.log(confirm_password);
-
-    setUsername("");
-    setEmail("");
-    setAddress("");
-    setContactInfo("");
-    setPassword("");
-    setConfirmPassword("");
+  const submitForm = (data) => {
+    console.log(data);
+    reset();
   };
+
+  console.log(watch("username"));
+  console.log(watch("email"));
+  console.log(watch("address"));
+  console.log(watch("contact_info"));
+  console.log(watch("password"));
+  console.log(watch("confirm_password"));
 
   return (
     <div className="container mt-5">
@@ -40,82 +37,127 @@ const Register = () => {
                   <Form.Control
                     type="text"
                     placeholder="Enter username"
-                    value={username}
-                    name="username"
-                    onChange={(e) => {
-                      setUsername(e.target.value);
-                    }}
+                    {...register("username", { required: true, maxLength: 50 })}
                   />
+
+                  {errors.username && (
+                    <p className="text-danger small">Username is required</p>
+                  )}
+                  {errors.username?.type === "maxLength" && (
+                    <p className="text-danger small">
+                      Username is too long. Max 50 characters
+                    </p>
+                  )}
                 </Form.Group>
               </Col>
+
               <Col md={6} className="mb-3">
                 <Form.Group>
                   <Form.Label className="mb-2">Email</Form.Label>
                   <Form.Control
                     type="email"
                     placeholder="Enter email"
-                    value={email}
-                    name="email"
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
+                    {...register("email", {
+                      required: true,
+                      maxLength: 100,
+                    })}
                   />
+
+                  {errors.email && (
+                    <p className="text-danger small">Email is required</p>
+                  )}
+                  {errors.email?.type === "maxLength" && (
+                    <p className="text-danger small">
+                      Email is too long. Max 100 characters
+                    </p>
+                  )}
                 </Form.Group>
               </Col>
+
               <Col md={6} className="mb-3">
                 <Form.Group>
                   <Form.Label className="mb-2">Address</Form.Label>
                   <Form.Control
                     type="text"
                     placeholder="Enter address"
-                    value={address}
-                    name="address"
-                    onChange={(e) => {
-                      setAddress(e.target.value);
-                    }}
+                    {...register("address", {
+                      required: true,
+                    })}
                   />
+
+                  {errors.address && (
+                    <p className="text-danger small">Address is required</p>
+                  )}
                 </Form.Group>
               </Col>
+
               <Col md={6} className="mb-3">
                 <Form.Group>
                   <Form.Label className="mb-2">Contact Info</Form.Label>
                   <Form.Control
                     type="text"
                     placeholder="Enter contact info"
-                    value={contact_info}
-                    name="contact_info"
-                    onChange={(e) => {
-                      setContactInfo(e.target.value);
-                    }}
+                    {...register("contact_info", {
+                      required: true,
+                      maxLength: 100,
+                    })}
                   />
+
+                  {errors.contact_info && (
+                    <p className="text-danger small">
+                      Contact info is required
+                    </p>
+                  )}
+                  {errors.contact_info?.type === "maxLength" && (
+                    <p className="text-danger small">
+                      Contact info is too long. Max 100 characters
+                    </p>
+                  )}
                 </Form.Group>
               </Col>
+
               <Col md={6} className="mb-3">
                 <Form.Group>
                   <Form.Label className="mb-2">Password</Form.Label>
                   <Form.Control
                     type="password"
                     placeholder="Enter password"
-                    value={password}
-                    name="password"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
+                    {...register("password", { required: true, minLength: 6 })}
                   />
+
+                  {errors.password && (
+                    <p className="text-danger small">Password is required</p>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <p className="text-danger small">
+                      Password is too short. Min 6 characters
+                    </p>
+                  )}
                 </Form.Group>
               </Col>
+
               <Col md={6} className="mb-3">
                 <Form.Group>
                   <Form.Label className="mb-2">Confirm Password</Form.Label>
                   <Form.Control
                     type="password"
                     placeholder="Confirm password"
-                    value={confirm_password}
-                    name="confirm_password"
-                    onChange={(e) => {
-                      setConfirmPassword(e.target.value);
-                    }}
+                    {...register("confirm_password", {
+                      required: true,
+                      minLength: 6,
+                    })}
                   />
+
+                  {errors.confirm_password && (
+                    <p className="text-danger small">
+                      Confirm password is required
+                    </p>
+                  )}
+                  {errors.confirm_password?.type === "minLength" && (
+                    <p className="text-danger small">
+                      Confirm password is too short. Min 6 characters
+                    </p>
+                  )}
                 </Form.Group>
               </Col>
             </Row>
@@ -127,7 +169,7 @@ const Register = () => {
                 as="sub"
                 variant="primary"
                 type="submit"
-                onClick={submitForm}
+                onClick={handleSubmit(submitForm)}
               >
                 Sign Up
               </Button>
